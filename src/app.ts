dec91beeb28f7152789773a565021831595a2f27
Diego Antunez <113407581+dierez00@users.noreply.github.com>
2025-10-26 00:09:11 -0600

    Fix CORS whitelist entry formatting

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 const whitelist = [
   'http://localhost:5173',
   'http://localhost:8080',
-  'http://localhost:8081
+  'http://localhost:8081'
 ];
 
 interface CorsCallback {
